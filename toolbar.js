d803b4b1bbeddcbb3773e53b5b7715f56d3867e8
MrBusiness87 <bbartula87@gmail.com>
2022-04-22 17:34:12 -0400

    working on javascript for toolbar buttons

---
+++ toolbar.js
@@ -0,0 +1,5 @@
+// Current Date:
+var today=new Date();
+var date=today.getFullYear()+":"+today.getMinutes()+":"+today.getSeconds();
+document.getElementById("currentDate").value=time;
+Current Time
