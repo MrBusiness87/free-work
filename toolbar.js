a46fc39a5e7c0a8de1dff9da3617e2786cd03b95
MrBusiness87 <bbartula87@gmail.com>
2022-04-22 22:14:11 -0400

    worked on javascript for date and time

--- toolbar.js
+++ toolbar.js
@@ -1,5 +1,20 @@
+// Current Date
 // Current Date:
-var today=new Date();
-var date=today.getFullYear()+":"+today.getMinutes()+":"+today.getSeconds();
-document.getElementById("currentDate").value=time;
-Current Time
+{/* <input type="text" id="currentDate"> */}
+  var today = new Date();
+  var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+  document.getElementById("currentDate").value = date;
+// Current Time
+// Current Time:
+{/* <input type="text" id="currentTime"> */}
+  var today = new Date();
+  var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+  document.getElementById("currentTime").value = time;
+// Current Date and Time
+// Get Current Date and Time Both:
+{/* <input type="text" id="currentDateTime"> */}
+  var today = new Date();
+  var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+  var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+  var dateTime = date+' '+time;
+  document.getElementById("currentDateTime").value = dateTime;
