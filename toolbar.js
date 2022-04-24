e6f9dad604cc0862890416edaed408d5059f8846
MrBusiness87 <bbartula87@gmail.com>
2022-04-23 22:40:55 -0400

    to be continued ... date as alert on click

--- toolbar.js
+++ toolbar.js
@@ -1,20 +1,24 @@
-// Current Date
-// Current Date:
-{/* <input type="text" id="currentDate"> */}
-  var today = new Date();
-  var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-  document.getElementById("currentDate").value = date;
-// Current Time
-// Current Time:
-{/* <input type="text" id="currentTime"> */}
-  var today = new Date();
-  var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-  document.getElementById("currentTime").value = time;
-// Current Date and Time
-// Get Current Date and Time Both:
-{/* <input type="text" id="currentDateTime"> */}
-  var today = new Date();
-  var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-  var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-  var dateTime = date+' '+time;
-  document.getElementById("currentDateTime").value = dateTime;
+// // Current Date
+// // Current Date:
+// {/* <input type="text" id="currentDate"> */}
+//   var today = new Date();
+//   var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+//   document.getElementById("currentDate").value = date;
+// // Current Time
+// // Current Time:
+// {/* <input type="text" id="currentTime"> */}
+//   var today = new Date();
+//   var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+//   document.getElementById("currentTime").value = time;
+// // Current Date and Time
+// // Get Current Date and Time Both:
+// {/* <input type="text" id="currentDateTime"> */}
+//   var today = new Date();
+//   var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+//   var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+//   var dateTime = date+' '+time;
+//   document.getElementById("currentDateTime").value = dateTime;
+
+function Date() {
+  alert
+}
