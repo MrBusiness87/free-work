5d465bdf376198704ddf6e3f033398c5fa7495ce
MrBusiness87 <bbartula87@gmail.com>
2022-05-04 23:31:18 -0400

    applying bootstrap

--- toolbar.js
+++ toolbar.js
@@ -1,30 +1,4 @@
 function toolbar() {
-  document.getElementsByClassName("toolbar")= "<ul class='nav justify-content-center'>
-  <a class='navbar-brand' href='./about.html'>
-    <img src='./images/about.png' alt='ABOUT ME' title='ABOUT ME'>
-  </a>
-  <li class='nav-item'>
-    <h3>Start of Something new!</h3>
-  </li>
-  <li class='nav-item'>
-    <a class='nav-link active' aria-current='page' href='#' onclick='currentDate()'>TIME?</a>
-  </li>
-  <li class='nav-item'>
-    <a class='nav-link active' aria-current='page' href='#'>Active</a>
-  </li>
-  <li class='nav-item'>
-    <a class='nav-link' href='#'>Link1</a>
-  </li>
-  <!-- <li class='nav-item dropdown'>
-    <a class='nav-link dropdown-toggle' href='#' id='navbarDropdown' role='button' data-bs-toggle='dropdown' aria-expanded='false'>
-      Dropdown
-    </a>
-    <ul class='dropdown-menu' aria-labelledby='navbarDropdown'>
-      <li><a class='dropdown-item' href='#'>Action</a></li>
-      <li><a class='dropdown-item' href='#'>Another action</a></li>
-      <li><hr class='dropdown-divider'></li>
-      <li><a class='dropdown-item' href='#'>Something else here</a></li>
-    </ul>
-  </li> -->
-</ul>"
+  const element = document.getElementsByClassName("toolbar");
+  element.innerHTML="New Heading";
 }
